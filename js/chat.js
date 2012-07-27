94e3cd055c13ebdd4ae78c6691340916db1abee6
João Batista Neto <neto.joaobatista@gmail.com>
2012-07-27 01:20:28 -0300

    Adicionando referência ao QUnit e arquivo de testes

--- js/chat.js
+++ js/chat.js
@@ -14,8 +14,12 @@ chatDojo.chat = {
 		for (var i = 0, t = this.forms.length; i < t; ++i) {
 			histories[i] = this.forms[i].querySelector('.history');
 			messages[i] = this.forms[i].querySelector('.message');
-			messages[i].onkeyup = function() {
-			};
+			messages[i].onkeyup = (function(i, evt) {
+				if (evt.keyCode == 13) {
+					histories[i].text += this.value;
+					this.value = "";
+				}
+			}).bind(messages[i], i);
 		}
 	}
 };
