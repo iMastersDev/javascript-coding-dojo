badf266bd64bb0a4f2b7cf8916e3c34a644b376e
João Batista Neto <neto.joaobatista@gmail.com>
2012-07-27 01:05:54 -0300

    iteração dos forms

--- js/chat.js
+++ js/chat.js
@@ -4,6 +4,18 @@
 
 var chatDojo = chatDojo || {};
 
-chatDojo.history = document.querySelectorAll('.history');
-chatDojo.message = document.querySelectorAll('.message');
-
+chatDojo.chat = {
+	start : function() {
+		var messages = [];
+		var histories = [];
+		
+		this.forms = document.querySelectorAll('.chat');
+		
+		for (var i = 0, t = this.forms.length; i < t; ++i) {
+			histories[i] = this.forms[i].querySelector('.history');
+			messages[i] = this.forms[i].querySelector('.message');
+			messages[i].onkeyup = function() {
+			};
+		}
+	}
+};
