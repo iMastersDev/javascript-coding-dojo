f531e0782773fe2167790a0ae7267e1e5d890f93
João Batista Neto <neto.joaobatista@gmail.com>
2012-07-27 01:32:28 -0300

    Ajuste no submit

--- js/chat.js
+++ js/chat.js
@@ -14,12 +14,13 @@ chatDojo.chat = {
 		for (var i = 0, t = this.forms.length; i < t; ++i) {
 			histories[i] = this.forms[i].querySelector('.history');
 			messages[i] = this.forms[i].querySelector('.message');
-			messages[i].onkeyup = (function(i, evt) {
-				if (evt.keyCode == 13) {
-					histories[i].text += this.value;
-					this.value = "";
-				}
-			}).bind(messages[i], i);
+			
+			this.forms[i].onsubmit = (function(i, evt){
+				histories[i].text += messages[i].value;
+				messages[i].value = "";
+				
+				return false;
+			}).bind(undefined, i);
 		}
 	}
 };
